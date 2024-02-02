7a745ca90616885ef0551b52a1d78752ba6526a4
ErkanDemir0 <122565958+erkanndemirr@users.noreply.github.com>
2024-02-02 19:49:49 +0300

    Commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ const londrina = Londrina_Shadow({
 const Header = () => {
   return (
     <div className='flex flex-col gap-4 justify-center items-center h-[90vh]'>
-        <h1 className='text-5xl font-londrina '>Blog.</h1>
+        <h1 className='text-9xl font-bold max-sm:text-7xl'>&lt;Blog.&gt;</h1>
     </div>
   )
 }
