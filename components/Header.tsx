a4b6a459c11ae04633f7be50224dd93a53fc40b4
ErkanDemir0 <122565958+erkanndemirr@users.noreply.github.com>
2024-02-03 01:26:35 +0300

    Commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,14 +12,13 @@ const londrina = Londrina_Shadow({
 const Header = () => {
   return (
     <div className="flex flex-col  justify-center items-center h-[100vh] w-full bg-center bg-[url('/2.jpg')] bg-no-repeat bg-cover pb-20">
-
       <div className='flex gap-2 text-white '>
         <p className=' text-[4rem]'>.</p>
-        <p className=' pt-[2.7rem] text-[2rem] font-bold max-sm:text-2xl'> Erkan Demir </p>
+        <p className=' pt-[2.7rem] text-[2rem] font-bold max-sm:text-2xl'>Erkan Demir</p>
         <p className='text-[4rem]'>.</p>
       </div>
       <h1 className='text-white text-[10rem] font-bold max-sm:text-7xl -mt-10'>&lt;Blog.&gt;</h1>
-      <div  className='text-white text-2xl'>"Colors of Life: <TypeAnimation sequence={[
+      <div  className='text-white text-2xl'>"Colors of Life:<TypeAnimation sequence={[
         // Same substring at the start will only be typed out once, initially
         'Thoughts',
         1000, // wait 1s before replacing "Mice" with "Hamsters"
