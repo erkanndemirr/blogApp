04e7da7021369d4f25646f1c37ef3f8146652cf0
ErkanDemir0 <122565958+erkanndemirr@users.noreply.github.com>
2024-02-03 17:42:55 +0300

    Commit

--- components/PostView.tsx
+++ components/PostView.tsx
@@ -7,7 +7,7 @@ const PostView = () => {
             <div className='flex justify-center items-center h-10'>
                 <h1 className='text-5xl font-bold border-b-2  max-sm:px-[5rem] px-[10rem] pb-5'>Posts</h1>
             </div>
-            <div className='w-full mt-10'>
+            <div className='w-full mt-10 max-sm:-ml-4'>
              
                     <Card />
         
