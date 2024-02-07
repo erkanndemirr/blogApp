134c21290d1b022ff0e40c810db57c93ba9b3c21
ErkanDemir0 <122565958+erkanndemirr@users.noreply.github.com>
2024-02-08 02:05:16 +0300

    Commit

--- components/Card.tsx
+++ components/Card.tsx
@@ -24,7 +24,7 @@ const Card = async () => {
                 {posts.map((post) => (
                     <li className="mb-10 ms-4 border rounded-lg p-2" key={post.id}>
                         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <p className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2024</p>
+                        <p className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 202</p>
                         <h3 className="text-xl font-semibold text-gray-900 dark:text-white">{post.title}</h3>
                         <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">{post.content}</p>
                         {post.published?  <Link href={`/blogs/${post.id}`} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more </Link>
